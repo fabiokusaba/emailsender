ccdd6a39f60e9498427fd8e7a4868f4da8898785
Fabio Kusaba <fabinhokb44@gmail.com>
2025-10-30 21:39:02 -0300

    test: adicionar testes unitários de validação do dominio

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -7,15 +7,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	name     = "Campaign X"
+	content  = "Content of Campaign X"
+	contacts = []string{"johndoe@gmail.com", "fizzbuzz@gmail.com"}
+	now      = time.Now().Add(-time.Minute)
+)
+
 func TestNewCampaign(t *testing.T) {
 	assert := assert.New(t)
 
-	name := "Campaign X"
-	content := "Content of Campaign X"
-	contacts := []string{"johndoe@gmail.com", "fizzbuzz@gmail.com"}
-	now := time.Now().Add(-time.Minute)
-
-	campaign := NewCampaign(name, content, contacts)
+	campaign, _ := NewCampaign(name, content, contacts)
 
 	assert.NotNil(campaign.ID)
 	assert.Equal(campaign.Name, name)
@@ -23,4 +25,28 @@ func TestNewCampaign(t *testing.T) {
 	assert.Equal(len(campaign.Contacts), len(contacts))
 	assert.NotNil(campaign.CreatedOn)
 	assert.Greater(campaign.CreatedOn, now)
+}
+
+func TestNewCampaignMustValidateName(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign("", content, contacts)
+
+	assert.Equal("name is required", err.Error())
+}
+
+func TestNewCampaignMustValidateContent(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, "", contacts)
+
+	assert.Equal("content is required", err.Error())
+}
+
+func TestNewCampaignMustValidateContacts(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, content, []string{})
+
+	assert.Equal("list of contacts is required", err.Error())
 }
