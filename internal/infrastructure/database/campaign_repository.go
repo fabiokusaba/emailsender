b19cfd4e494ee4f7e0d0e42a45384fd25f53dd3d
Fabio Kusaba <fabinhokb44@gmail.com>
2025-12-24 20:17:27 -0300

    feat: adicionar campaign repository

---
+++ internal/infrastructure/database/campaign_repository.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"github.com/fabiokusaba/emailsender/internal/domain/campaign"
+)
+
+type CampaignRepository struct {
+	campaigns []campaign.Campaign
+}
+
+func (cr *CampaignRepository) Save(campaign *campaign.Campaign) error {
+	cr.campaigns = append(cr.campaigns, *campaign)
+	return nil
+}
