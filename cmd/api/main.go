a1cac3d9eac40aabc6ffdf1470d29aa723b62a80
Fabio Kusaba <fabinhokb44@gmail.com>
2025-12-24 20:00:40 -0300

    feat: adicionar post campaign

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"net/http"
 
+	"github.com/fabiokusaba/emailsender/internal/contract"
+	"github.com/fabiokusaba/emailsender/internal/domain/campaign"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
 func main() {
@@ -15,8 +18,27 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+	service := campaign.Service{}
+
+	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		var request contract.NewCampaign
+
+		err := render.DecodeJSON(r.Body, &request)
+		if err != nil {
+			render.Status(r, 500)
+			render.JSON(w, r, map[string]string{"error": err.Error()})
+			return
+		}
+
+		id, err := service.Create(request)
+		if err != nil {
+			render.Status(r, 400)
+			render.JSON(w, r, map[string]string{"error": err.Error()})
+			return
+		}
+
+		render.Status(r, 201)
+		render.JSON(w, r, map[string]string{"id": id})
 	})
 
 	http.ListenAndServe(":3000", r)
