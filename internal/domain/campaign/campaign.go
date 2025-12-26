35d73d89987226da072f7445eb429d867b617de8
Fabio Kusaba <fabinhokb44@gmail.com>
2025-12-26 11:23:51 -0300

    feat: adicionar campo Status

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -11,12 +11,19 @@ type Contact struct {
 	Email string `validate:"email"`
 }
 
+const (
+	Pending string = "Pending"
+	Started string = "Started"
+	Done    string = "Done"
+)
+
 type Campaign struct {
 	ID        string    `validate:"required"`
 	Name      string    `validate:"min=5,max=24"`
 	CreatedOn time.Time `validate:"required"`
 	Content   string    `validate:"min=5,max=1024"`
 	Contacts  []Contact `validate:"min=1,dive"`
+	Status    string
 }
 
 func NewCampaign(name, content string, emails []string) (*Campaign, error) {
@@ -32,6 +39,7 @@ func NewCampaign(name, content string, emails []string) (*Campaign, error) {
 		Content:   content,
 		CreatedOn: time.Now(),
 		Contacts:  contacts,
+		Status:    Pending,
 	}
 
 	err := validator.ValidateStruct(campaign)
