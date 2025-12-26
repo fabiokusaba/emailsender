f16a1b3f54eac321af87894ceb3d0363b24dd7ee
Fabio Kusaba <fabinhokb44@gmail.com>
2025-12-26 09:57:47 -0300

    test: adicionar teste de retorno do objeto e status

--- internal/endpoints/handler_error_test.go
+++ internal/endpoints/handler_error_test.go
@@ -1,6 +1,7 @@
 package endpoints
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -40,4 +41,27 @@ func TestHandlerError_when_endpoint_returns_domain_error(t *testing.T) {
 
 	assert.Equal(http.StatusBadRequest, res.Code)
 	assert.Contains(res.Body.String(), "domain error")
+}
+
+func TestHandlerError_when_endpoint_returns_obj_and_status(t *testing.T) {
+	assert := assert.New(t)
+	type BodyForTest struct {
+		Id int
+	}
+
+	objExpected := BodyForTest{Id: 1}
+	var objReturned BodyForTest
+
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return objExpected, 200, nil
+	}
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	json.Unmarshal(res.Body.Bytes(), &objReturned)
+	assert.Equal(objExpected, objReturned)
 }
