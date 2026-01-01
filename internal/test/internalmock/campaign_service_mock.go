ea6701eb4ad666a8a9674b9737b9229a3cad9611
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-01 20:29:47 -0300

    feat: adicionar mock para camada de servico

---
+++ internal/test/internalmock/campaign_service_mock.go
@@ -0,0 +1,25 @@
+package internalmock
+
+import (
+	"github.com/fabiokusaba/emailsender/internal/contract"
+	"github.com/stretchr/testify/mock"
+)
+
+type CampaignServiceMock struct {
+	mock.Mock
+}
+
+func (s *CampaignServiceMock) Create(newCampaign contract.NewCampaign) (string, error) {
+	args := s.Called(newCampaign)
+	return args.String(0), args.Error(1)
+}
+
+func (s *CampaignServiceMock) GetById(id string) (*contract.CompaignResponse, error) {
+	args := s.Called(id)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*contract.CompaignResponse), nil
+}
