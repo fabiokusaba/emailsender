f4d4a64ee06c26d07cb9cb98f148387f26b9ce25
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-01 21:23:16 -0300

    feat: implementar gorm no repositorio

--- internal/infrastructure/database/campaign_repository.go
+++ internal/infrastructure/database/campaign_repository.go
@@ -2,17 +2,26 @@ package database
 
 import (
 	"github.com/fabiokusaba/emailsender/internal/domain/campaign"
+	"gorm.io/gorm"
 )
 
 type CampaignRepository struct {
-	campaigns []campaign.Campaign
+	Db *gorm.DB
 }
 
 func (cr *CampaignRepository) Save(campaign *campaign.Campaign) error {
-	cr.campaigns = append(cr.campaigns, *campaign)
-	return nil
+	tx := cr.Db.Create(campaign)
+	return tx.Error
 }
 
 func (cr *CampaignRepository) GetAll() ([]campaign.Campaign, error) {
-	return cr.campaigns, nil
+	var campaigns []campaign.Campaign
+	tx := cr.Db.Find(&campaigns)
+	return campaigns, tx.Error
+}
+
+func (cr *CampaignRepository) GetById(id string) (*campaign.Campaign, error) {
+	var campaign campaign.Campaign
+	tx := cr.Db.First(&campaign, id)
+	return &campaign, tx.Error
 }
