2044858c00499b63ccc1b98b21faa16743f88ec0
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-02 10:18:39 -0300

    feat: adicionar migrations

--- internal/infrastructure/database/postgres.go
+++ internal/infrastructure/database/postgres.go
@@ -1,15 +1,23 @@
 package database
 
 import (
+	"github.com/fabiokusaba/emailsender/internal/domain/campaign"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDb() *gorm.DB {
 	dsn := "host=localhost port=5432 user=postgres password=pg123 dbname=emailsender sslmode=disable"
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
+
+	err = db.AutoMigrate(&campaign.Campaign{}, &campaign.Contact{})
+	if err != nil {
+		panic("failed to migrate models")
+	}
+
 	return db
 }
