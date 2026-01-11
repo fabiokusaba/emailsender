5ee6aa90a1ad4f1638ccd6e7421ecfb7541772af
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-11 19:34:08 -0300

    feat: adicionar mock para campaign repository

---
+++ internal/test/internalmock/campaign_repository_mock.go
@@ -0,0 +1,39 @@
+package internalmock
+
+import (
+	"github.com/fabiokusaba/emailsender/internal/domain/campaign"
+	"github.com/stretchr/testify/mock"
+)
+
+type CampaignRepositoryMock struct {
+	mock.Mock
+}
+
+func (r *CampaignRepositoryMock) Save(campaign *campaign.Campaign) error {
+	args := r.Called(campaign)
+	return args.Error(0)
+}
+
+func (r *CampaignRepositoryMock) Update(campaign *campaign.Campaign) error {
+	args := r.Called(campaign)
+	return args.Error(0)
+}
+
+func (r *CampaignRepositoryMock) GetAll() ([]campaign.Campaign, error) {
+	return []campaign.Campaign{}, nil
+}
+
+func (r *CampaignRepositoryMock) GetById(id string) (*campaign.Campaign, error) {
+	args := r.Called(id)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*campaign.Campaign), nil
+}
+
+func (r *CampaignRepositoryMock) Delete(campaign *campaign.Campaign) error {
+	args := r.Called(campaign)
+	return args.Error(0)
+}
