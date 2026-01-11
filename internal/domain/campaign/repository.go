0fe653141262f088c1229ec96c98bfa5f5e2b38b
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-11 18:19:04 -0300

    feat: adicionar metodos Update e Delete

--- internal/domain/campaign/repository.go
+++ internal/domain/campaign/repository.go
@@ -2,6 +2,8 @@ package campaign
 
 type Repository interface {
 	Save(campaign *Campaign) error
+	Update(campaign *Campaign) error
 	GetAll() ([]Campaign, error)
 	GetById(id string) (*Campaign, error)
+	Delete(campaign *Campaign) error
 }
