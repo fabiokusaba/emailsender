5f24f8146dc234e3a5b7fbea698a21d7d86f8930
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-11 11:02:26 -0300

    feat: adicionar handler para cancelar campaign

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,6 +24,7 @@ func main() {
 
 	r.Post("/campaigns", endpoints.HandlerError(handler.PostCampaign))
 	r.Get("/campaigns/{id}", endpoints.HandlerError(handler.GetCampaignById))
+	r.Patch("/campaigns/cancel/{id}", endpoints.HandlerError(handler.CampaignsCancelPatch))
 
 	http.ListenAndServe(":3000", r)
 }
