54be98bdecf0c82c98016f10bbb18b52e6b84217
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-20 20:56:13 -0300

    feat: adicionar rota para checar a aplicacao e agrupar as rotas de campaigns

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/fabiokusaba/emailsender/internal/infrastructure/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
 func main() {
@@ -17,16 +18,23 @@ func main() {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(endpoints.Auth)
 
 	db := database.NewDb()
 	campaignService := campaign.ServiceImpl{Repository: &database.CampaignRepository{Db: db}}
 	handler := endpoints.Handler{CampaignService: &campaignService}
 
-	r.Post("/campaigns", endpoints.HandlerError(handler.PostCampaign))
-	r.Get("/campaigns/{id}", endpoints.HandlerError(handler.GetCampaignById))
-	r.Patch("/campaigns/cancel/{id}", endpoints.HandlerError(handler.CampaignsCancelPatch))
-	r.Delete("/campaigns/delete/{id}", endpoints.HandlerError(handler.DeleteCampaign))
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		render.Status(r, 200)
+		render.JSON(w, r, map[string]string{"health": "OK!"})
+	})
+
+	r.Route("/campaigns", func(r chi.Router) {
+		r.Use(endpoints.Auth)
+		r.Post("/", endpoints.HandlerError(handler.PostCampaign))
+		r.Get("/{id}", endpoints.HandlerError(handler.GetCampaignById))
+		r.Patch("/cancel/{id}", endpoints.HandlerError(handler.CampaignsCancelPatch))
+		r.Delete("/delete/{id}", endpoints.HandlerError(handler.DeleteCampaign))
+	})
 
 	http.ListenAndServe(":3000", r)
 }
