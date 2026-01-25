8c74287ed0fa7eee665fb9e50b6ed016e613fba8
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-25 11:13:38 -0300

    feat: carregar arquivo .env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/fabiokusaba/emailsender/internal/domain/campaign"
@@ -9,9 +10,15 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
