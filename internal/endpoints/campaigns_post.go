09958a21e936d10c8c1759b077ec7d846ce01416
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-25 10:30:36 -0300

    feat: adicionar email obtido pelo contexto na propriedade CreatedBy

--- internal/endpoints/campaigns_post.go
+++ internal/endpoints/campaigns_post.go
@@ -9,10 +9,9 @@ import (
 
 func (h *Handler) PostCampaign(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
 	var request contract.NewCampaign
-
 	err := render.DecodeJSON(r.Body, &request)
-
+	email := r.Context().Value("email").(string)
+	request.CreatedBy = email
 	id, err := h.CampaignService.Create(request)
-
 	return map[string]string{"id": id}, 201, err
 }
